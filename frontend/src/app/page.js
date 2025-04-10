befd8f952295e15afe88ab14a930ff51b05c1363
SalimBinYousuf1 <salim.bin.yousuf2@gmail.com>
2025-04-10 13:16:43 +0530

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,8 +2,6 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
-
 export default function HomePage() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
